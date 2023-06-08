dbe4ead95d6ba2f8e18fe88402386ef7aac11fd1
KosukeNakagawa <kosukenakagawa730@gmail.com>
2023-06-09 01:03:40 +0900

    first commit

---
+++ ContainerView.sample/ContainerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ContainerViewController.swift
+//  ContainerView.sample
+//
+//  Created by 中川こうすけ on 2023/06/09.
+//
+
+import UIKit
+
+class ContainerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.yellow
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
